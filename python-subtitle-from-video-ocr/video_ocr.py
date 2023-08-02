236c8cfd3d331860d9189d0179f6dee01b8d151a
hleo1 <leo.hubert3@gmail.com>
2023-08-02 16:09:01 -0400

    reorganized files

--- python-subtitle-from-video-ocr/video_ocr.py
+++ python-subtitle-from-video-ocr/video_ocr.py
@@ -3,7 +3,7 @@ import cv2
 import logging
 logging.disable(logging.CRITICAL)
 import re
-
+import os
 # # If you want to use GPU (use_gpu=True), make sure to have the paddlepaddle-gpu library installed
 # ocr = PaddleOCR(use_gpu=False) 
 # img_path = 'Capture.PNG' # replace 'test.jpg' with your image
@@ -24,11 +24,9 @@ def ocr_output(image_name) :
     ocr = PaddleOCR(use_gpu=False) 
     img_path = image_name # replace 'test.jpg' with your image
     result = ocr.ocr(img_path)
-
     for line in result:
         for word_info in line:
-            print(image_name + " is: " + remove_special_chars(word_info[-1][0]) + " confidence level is: " + str(word_info[-1][1]))
-            return remove_special_chars(word_info[-1][0])
+            return word_info[-1]
 
 # Open video file
 video = cv2.VideoCapture('subtitle extractor test.mp4')
@@ -42,15 +40,24 @@ frame_rate = video.get(cv2.CAP_PROP_FPS)
 # 25.0 frames per second
 frame_count = 0
 
+if not os.path.exists('assets'):
+    os.makedirs('assets')
+
 while video.isOpened():
     # Capture frame-by-frame
     ret, frame = video.read()
     
     if ret:
-        if frame_count % (frame_rate // 4) == 0:  # Adjust as needed to change screenshot frequency
+        if frame_count % (frame_rate // 5) == 0:  # Adjust as needed to change screenshot frequency
             # Save frame as image
-            cv2.imwrite(f'frame{frame_count}.jpg', frame)
-            ocr_output(f'frame{frame_count}.jpg')
+            image_name = f'assets/frame{frame_count}.jpg'
+            cv2.imwrite(image_name, frame)
+            result = ocr_output(image_name)
+            if (result != None) :
+                print(image_name + " is: " + remove_special_chars(result[-1][0]) + " confidence level is: " + str(result[-1][1]))
+
+            else:
+                print("nothing in frame")
     else:
         break
 
